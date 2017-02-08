528c52382c114d1edc733263f4530048c00f8ac9
Marco Roganovic <marcoroganovic@gmail.com>
2017-02-08 15:37:15 +0100

    improve demo

--- demo/demo.js
+++ demo/demo.js
@@ -46,9 +46,52 @@
   autoType({
     content: text,
     element: $el,
-    perChar: 100,
+    perChar: 50,
     cursor: "_"
   });
-  
+
+
+  var textTwo = `
+  const TodoItem = (props) => {
+    return <li data-id="{props.id}">{props.task}</li>;
+  }
+
+  class TodoList extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        todos: [
+          { id: 1, task: "Learn JavaScript", state: false },
+          { id: 2, task: "Practice a lot", state: false },
+          { id: 3, task: "Build something great", state: false }
+        ]
+      }
+    }
+
+    render() {
+      var list = this.state.todos.map(todo => {
+        return <TodoItem id="{todo.id}" task="{todo.task}" />
+      });
+
+      return (
+          <section className="todo-container">
+            {list}
+          </section>
+      );
+    }
+  }
+
+  var $root = document.querySelector("#app");
+  ReactDOM.render(<TodoList />, $root);
+  `;
+
+  var $elTwo = document.querySelector(".code-two");
+
+  autoType({
+    content: textTwo,
+    element: $elTwo,
+    perChar: 100,
+    cursor: "|"
+  });
 
 })();
